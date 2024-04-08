462b435ffd8bc10928b16352a96c2b4ec0da15c9
Pavel Novozhilov <p.novozhilov24@mail.ru>
2024-04-08 14:46:04 +0300

    Создание файла и реализация программы на языке С#

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿int getCountLitleWord(string[] words) {
+    int count = 0;
+    foreach (string word in words)
+    {
+        if (word.Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+string[] getArrayLitleWord(string[] words)
+{
+    int length = getCountLitleWord(words);
+    int index = 0;
+    string[] result = new string[length];
+    foreach (string word in words)
+    {
+        if (word.Length <= 3)
+        {
+           result[index] = word;
+           index++;
+        }
+    }
+    return result;
+}
+
+void printArray(string[] words)
+{
+    foreach (string word in words)
+    {
+        Console.Write(word + " ");
+    }
+}
+
+
+Console.WriteLine("Введите количество слов");
+int countWords = int.Parse(Console.ReadLine()!);
+string[] inputListWords = new string[countWords];
+for (int i = 0; i < countWords; i++)
+{
+    inputListWords[i] = Console.ReadLine()!;
+
+}
+Console.WriteLine();
+
+string[] arrayLitleWords = getArrayLitleWord(inputListWords);
+printArray(arrayLitleWords);
